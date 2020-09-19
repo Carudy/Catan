2d5771597f76a22faa3d0906fb98895ad716a5b6
dy <dymain@126.com>
2020-09-19 12:50:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,186 @@
+// const server_addr = '10.28.156.99'
+const server_addr = '10.147.19.5'
+// const server_addr = '10.147.17.162'
+const server_port = '5659'
+var   server_url = 'http://' + server_addr + ':' + server_port
+var   ter = 100
+var cd = {
+        'beat' : 0,
+        'chat' : 0,
+        'info' : 0,
+        'others' : 0,
+    }
+var beat_fail = 0
+var player_id = 0, play_id = 0, playing = 0
+var chat_id = 0
+
+var c = document.getElementById("canva")
+var cxt = c.getContext("2d")
+cxt.fillStyle = "#eeaa22"
+
+var sdot = [0, 0], action = 'road', dot_cnt = 0
+var catan_map = []
+
+
+$(() => { 
+    $('#nick').val('DY')
+    $('#addr').val(server_addr)
+    $('#port').val(server_port)
+    // $('#type').val(0)
+    // setTimeout(draw_map, 100)
+
+    $('#link').click(()=>{
+        // player_type = $('#type').val()
+        server_url = 'http://' + $('#addr').val() + ':' + $('#port').val()
+        send({
+                'cmd' : 'reg',
+                'name' : $('#nick').val(),
+            }, res=>{
+                $('#info0').text('成功连接')
+                player_id = res.uid
+                beat_fail = 0
+            })
+    })
+
+    $('#speak').click(()=>{
+        if($('#speech').val().length==0){
+            $('#info2').text('消息不能为空！')
+            return
+        }
+
+        send({
+            'cmd' : 'say',
+            'uid' : player_id,
+            'cont' : $('#speech').val(),
+        }, res=>{})
+        $('#speech').val('')
+    })
+
+
+    $('#ready').click(()=>{
+        send({
+            'cmd' : 'map',
+        }, res=>{
+            catan_map = res.map
+            draw_map(catan_map)
+        })
+    })
+
+    // hot keys
+    $(window).on('keypress', function(e) {
+        if (e.keyCode ===13) $('#speak').trigger('click')
+    })
+
+    setInterval(god, ter)
+})
+
+
+//************************************************************* 
+send = (data, callback)=>{
+    $.post(server_url, JSON.stringify(data), (res)=>{
+        callback(res)
+    })
+}
+
+select_dot = (str) => {
+    let dot = str.split('_')
+    sdot[dot_cnt] = [Number(dot[0]), Number(dot[1])]
+    if(action=='road'){
+        if(dot_cnt == 1) console.log(sdot[0], sdot[1], is_road(sdot[0], sdot[1]))
+        dot_cnt = (dot_cnt + 1) & 1
+    }
+}
+
+cd_run = (func, k, t) =>{
+    if (cd[k]>=t){
+        cd[k] = 0
+        func()
+    }
+}
+
+heart_beat = ()=>{
+    beat_fail += 1
+    send({'cmd' : 'beat', 'uid' : player_id}, res=>{
+        if(res.res=='ok'){
+            // $('#info0').text('成功连接')
+            beat_fail = 0
+        }else{
+            $('#info0').text('连接失败')
+            player_id = 0
+            player_type = 0
+        }
+    })
+    if(beat_fail>1){
+        $('#info0').text('连接失败')
+    }
+}
+
+ask_chat = ()=>{
+    send({'cmd' : 'ask_chat', 'from' : chat_id}, res=>{
+        if(res.n>0){
+            for (i in res.data) {
+                let cont = res.data[i][0] + ': ' + res.data[i][1]  
+                cont = '<div class="chat_box">' + cont + '</div>'              
+                $('#chat_board').html($('#chat_board').html() + cont)
+            }
+            chat_id += res.n
+            $('#chat_board').scrollTop(999999)
+        }
+    })
+}
+
+ask_others = ()=>{
+    send({'cmd' : 'others', 'uid' : player_id}, res=>{
+        if(res.n>0){
+            $('#others-list').html(res.user.join(''))
+        }
+    })
+}
+
+ask_info = () =>{
+    send({'cmd' : 'info', 'uid' : player_id}, res=>{
+        playing = res.playing
+        if (playing){
+            $('#info2').text('游戏已开始')
+        } else{
+            $('#info2').text('游戏未开始')
+        }
+
+        // user info
+        // $('#online-user').text(res.users)
+        // $('#credit').text(res.coin)
+
+        // new game
+        // if (res.play_id != play_id){
+        //     play_id = res.play_id
+        //     if (player_id == res.painter) {
+        //         player_type = 1
+        //         $('#info0').text('你是画家')
+        //         $('#info1').text('题目: ' + res.answer)
+        //     }else{
+        //         player_type = 0
+        //         $('#info0').text('你是猜测者')
+        //         $('#info1').text('猜一个成语')
+        //     }
+        // }
+
+    })
+}
+
+god = ()=>{
+    for(i in cd) cd[i] += ter 
+
+    // whether alive
+    if(player_id==0 || beat_fail>1) return
+
+    // heart_beat()
+    cd_run(heart_beat, 'beat', 2000)
+    
+    // work
+    cd_run(ask_chat, 'chat', 500)
+    cd_run(ask_others, 'others', 1000)
+    // ask_info()
+
+}
+
+
---
+++ map.js
@@ -0,0 +1,108 @@
+var origin_width = 173.20508075689, origin_height = 200.0
+var bili = 0.75
+var img_width = origin_width * bili, img_height = origin_height * bili
+var ox = 230, oy = 25
+var sx = [], sy = [oy, 0, 0, 0, 0]
+var dot_pos = {}, col = [0, 3, 4, 5, 4, 3]
+var offset = $('canvas').offset()
+offset.top  -= 10
+offset.left -= 10
+
+draw_img = (c, x, y) =>{
+    cxt.drawImage($('#' + c)[0], x, y, img_width, img_height)
+}
+
+hex_dot_pos = (a, b) => {
+    a -= 1
+    b -= 1
+    let x = sx[a] + b * img_width, y = sy[a]
+    tp = [x + img_width * 0.5, y]
+    l1 = [x, y + img_height * 0.25]
+    l2 = [x, y + img_height * 0.75]
+    r1 = [x + img_width, y + img_height * 0.25]
+    r2 = [x + img_width, y + img_height * 0.75]
+    bt = [x + img_width * 0.5, y + img_height]
+    return [tp, l1, l2, r1, r2, bt]
+}
+
+hex_dot = (a, b) => {
+    tp  = [2*a-1 , b + (a>3?1:0)]
+    l1  = [2*a   , tp[1] - (a>3?1:0)]
+    l2  = [2*a+1 , l1[1]]
+    r1  = [2*a   , l1[1] + 1]
+    r2  = [2*a+1 , l1[1] + 1]
+    bt  = [2*a+2 , tp[1] + (a<3?1:(a>3?-1:0))]
+
+    return [tp, l1, l2, r1, r2, bt]
+}
+
+cal_dot_pos = () => {
+    for(let a=1; a<=5; ++a){
+        for(let b=1; b<=col[a]; ++b){
+            let cos = hex_dot(a, b)
+            let poses = hex_dot_pos(a, b)
+            for(let i in cos){
+                let key = cos[i].join('_')
+                if (key == '8_6') console.log(a, b)
+                if (! (key in dot_pos)) dot_pos[key] = poses[i]
+            }
+        }
+    }
+}
+
+set_map = () => {
+    sx = [ox, ox - 0.5 * img_width, ox - img_width, ox - 0.5 * img_width, ox]
+    for(let i=1; i<5; ++i) sy[i] = sy[i-1] + img_height * 0.75
+}
+
+draw_hex = (a, b) => {
+    for (let i of hex_dot_pos(a, b)){
+        cxt.beginPath()
+        cxt.arc(i[0], i[1], 5, 0, Math.PI*2);
+        cxt.closePath()
+        cxt.fill()
+    }
+}
+
+add_point_btn = () => {
+    for(let i in dot_pos) {
+        if(!Boolean($('#' + i)[0])){
+            $('#road').append('<button onclick="select_dot(\'' + i + '\')" class="dot" id="' + i + '"> </button>')
+            $('#' + i).offset({'top' : offset.top + dot_pos[i][1], 'left' : offset.left + dot_pos[i][0]})
+        }
+    }
+}
+
+is_road = (a, b) => {
+    if(a[0]==b[0]) return false
+    if(a[0]>b[0]) return is_road(b, a)
+    if(a[0]+1 != b[0]) return false
+    if((a[0]&1) == 0)return a[1] == b[1]
+    if(a[0]<6) return a[1]==b[1] || b[1]==a[1]+1
+    return a[1]==b[1] || a[1]==b[1]+1
+}
+
+n2cor = (n)=>{
+    if (n<=3) return [1, n]
+    if (n<=7) return [2, n-3]
+    if (n<=12) return [3, n-7]
+    if (n<=16) return [4, n-12]
+    return [5, n-16]
+}
+
+draw_map = (mp) =>{
+    console.log(mp)
+    set_map()
+    cal_dot_pos()
+    c.width = c.width
+    for(let i of mp){
+        let cor = n2cor(i[0])
+        draw_img('wood', sx[cor[0]-1] + (cor[1]-1) * img_width, sy[cor[0]-1])
+    }
+    // for(let i=0; i<3; ++i) draw_img('wood', sx[0] + i * img_width, sy[0])
+    // for(let i=0; i<4; ++i) draw_img('wood', sx[1] + i * img_width, sy[1])
+    // for(let i=0; i<5; ++i) draw_img('wood', sx[2] + i * img_width, sy[2])
+    // for(let i=0; i<4; ++i) draw_img('wood', sx[3] + i * img_width, sy[3])
+    // for(let i=0; i<3; ++i) draw_img('wood', sx[4] + i * img_width, sy[4])
+    add_point_btn()
+}
